362025eba297378d20e66ff3aae1f5976815372b
Denis Nikitin <dinya.nikitin@gmail.com>
2022-05-26 11:12:19 +0300

    change HOC

--- hm_7-1/src/components/Video.js
+++ hm_7-1/src/components/Video.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import DateTimePretty from './DateTimePretty'
+import DateTime from './DateTime'
 
 function Video(props) {
+    const VideoWithPretty = DateTimePretty(DateTime)
     return (
         <div className="video">
             <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <DateTimePretty date={props.date} />
+            <VideoWithPretty date={props.date} />
         </div>
     )
 }
